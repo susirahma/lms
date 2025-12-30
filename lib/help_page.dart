4dc52593331250af0935c3c4ff7b6f5bf8e18517
susirahma <susirahma292@gmail.com>
2025-12-30 22:04:01 +0700

    menambahkan deskripsi di EN

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -11,6 +11,28 @@ class HelpPage extends StatefulWidget {
 class _HelpPageState extends State<HelpPage> {
   String selectedLanguage = 'ID';
 
+  final Map<String, Map<String, String>> texts = {
+    'ID': {
+      'header': 'Akses hanya untuk Dosen dan Mahasiswa Telkom University.',
+      'instruction': 'Detail format login Microsoft Office 365:',
+      'username': 'Username (Akun iGracias) + \'@365.telkomuniversity.ac.id\'',
+      'password': 'Informasi mengenai kewajiban \'Strong Password\' di iGracias.',
+      'footer': 'Contact:',
+      'email': 'Email: infoceloe@telkomuniversity.ac.id',
+      'whatsapp': 'WhatsApp: +62 821-1666-3563',
+    },
+    'EN': {
+      'header': 'Access restricted only for Lecturer and Student of Telkom University.',
+      'instruction': 'Login only using your Microsoft Office 365 Account by following these format :',
+      'username': 'Username (iGracias Account) followed with \'@365.telkomuniversity.ac.id\'',
+      'password': 'Password (SSO / iGracias Account) on Password Field.',
+      'warning': 'Failure upon Authentication could be possibly you have not yet change your password into \'Strong Password\'. Make sure to change your Password only in iGracias.',
+      'footer': 'For further Information, please contact CeLOE Service Helpdesk :',
+      'email': 'Email: infoceloe@telkomuniversity.ac.id',
+      'whatsapp': 'WhatsApp: +62 821-1666-3563',
+    },
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +60,20 @@ class _HelpPageState extends State<HelpPage> {
                   },
                   child: Column(
                     children: [
+                      Image.network(
+                        'https://flagcdn.com/w320/id.png',
+                        width: 50,
+                        height: 30,
+                        fit: BoxFit.cover,
+                      ),
                       Text(
                         'ID',
-                        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
+                        style: GoogleFonts.poppins(fontSize: 14),
                       ),
                       if (selectedLanguage == 'ID')
                         Container(
-                          height: 2,
-                          width: 30,
+                          height: 3,
+                          width: 50,
                           color: const Color(0xFFB71C1C),
                         ),
                     ],
@@ -60,14 +88,20 @@ class _HelpPageState extends State<HelpPage> {
                   },
                   child: Column(
                     children: [
+                      Image.network(
+                        'https://flagcdn.com/w320/gb.png',
+                        width: 50,
+                        height: 30,
+                        fit: BoxFit.cover,
+                      ),
                       Text(
                         'EN',
-                        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
+                        style: GoogleFonts.poppins(fontSize: 14),
                       ),
                       if (selectedLanguage == 'EN')
                         Container(
-                          height: 2,
-                          width: 30,
+                          height: 3,
+                          width: 50,
                           color: const Color(0xFFB71C1C),
                         ),
                     ],
@@ -77,38 +111,63 @@ class _HelpPageState extends State<HelpPage> {
             ),
             const SizedBox(height: 30),
             // Content
-            Text(
-              'Akses hanya untuk Dosen dan Mahasiswa Telkom University.',
-              style: GoogleFonts.poppins(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Detail format login Microsoft Office 365:',
-              style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Username (Akun iGracias) + \'@365.telkomuniversity.ac.id\'',
-              style: GoogleFonts.poppins(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Informasi mengenai kewajiban \'Strong Password\' di iGracias.',
-              style: GoogleFonts.poppins(fontSize: 16),
-            ),
-            const SizedBox(height: 30),
-            // Contact
-            Text(
-              'Contact:',
-              style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Email: infoceloe@telkomuniversity.ac.id',
-              style: GoogleFonts.poppins(fontSize: 16),
-            ),
-            Text(
-              'WhatsApp: +62 821-1666-3563',
-              style: GoogleFonts.poppins(fontSize: 16),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              child: Column(
+                key: ValueKey<String>(selectedLanguage),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    texts[selectedLanguage]!['header']!,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    texts[selectedLanguage]!['instruction']!,
+                    style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    texts[selectedLanguage]!['username']!,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    texts[selectedLanguage]!['password']!,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                  if (selectedLanguage == 'EN' && texts[selectedLanguage]!['warning'] != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Text(
+                        texts[selectedLanguage]!['warning']!,
+                        style: GoogleFonts.poppins(fontSize: 16),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  const SizedBox(height: 30),
+                  // Contact
+                  Text(
+                    texts[selectedLanguage]!['footer']!,
+                    style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    texts[selectedLanguage]!['email']!,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    texts[selectedLanguage]!['whatsapp']!,
+                    style: GoogleFonts.poppins(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
